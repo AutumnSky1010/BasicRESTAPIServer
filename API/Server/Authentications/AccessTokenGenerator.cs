6a82336577f3e2d2dd22af514df307d55870af22
AutumnSky1010 <66455966+AutumnSky1010@users.noreply.github.com>
2024-11-27 01:33:37 +0900

    :+1: ユーザIDをリクエストヘッダから解析する処理を実装

--- API/Server/Authentications/AccessTokenGenerator.cs
+++ API/Server/Authentications/AccessTokenGenerator.cs
@@ -46,6 +46,24 @@ public class AccessTokenGenerator(string key, string issuer, string audience) : 
         return tokenString;
     }
 
+    /// <summary>
+    /// リクエストからユーザIDを解析する
+    /// </summary>
+    /// <param name="authHeader">認可ヘッダ文字列</param>
+    /// <returns>ユーザID</returns>
+    public static Guid ParseUserId(HttpRequest request)
+    {
+        var authHeader = request.Headers.Authorization.ToString();
+        var handler = new JwtSecurityTokenHandler();
+        // 認可ヘッダには「Bearer 」という接頭辞がついているため、取り除く
+        authHeader = authHeader.Replace("Bearer ", "");
+        var jwtSecurityToken = (JwtSecurityToken)handler.ReadToken(authHeader);
+        // サブジェクトクレームにユーザIDがある
+        var id = jwtSecurityToken.Claims.First(claim => claim.Type == JwtRegisteredClaimNames.Sub).Value;
+        // Guidとして解析し戻す
+        return Guid.Parse(id);
+    }
+
     /**
      * [参考]
      * https://qiita.com/te-k/items/600df0d0d812139de881
