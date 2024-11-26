38c0e1c68f80ad311ced602b3ac28a59146c905b
AutumnSky1010 <66455966+AutumnSky1010@users.noreply.github.com>
2024-11-26 15:05:09 +0900

    :+1: アクセストークンの生成処理を実装

---
+++ API/Server/Authentications/AccessTokenGenerator.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Server.Models.Users;
+using Server.UseCases.UserAuthentications;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace Server.Authentications;
+public class AccessTokenGenerator(string key, string issuer, string audience) : IAccessTokenGenerator
+{
+    private readonly string _key = key;
+
+    private readonly string _issuer = issuer;
+
+    private readonly string _audience = audience;
+
+    /// <summary>
+    /// トークンを生成する。
+    /// </summary>
+    /// <param name="userId">ユーザID</param>
+    /// <returns>トークン</returns>
+    public string Generate(UserId userId)
+    {
+        var secrityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_key));
+        // 資格情報
+        var credentials = new SigningCredentials(secrityKey, SecurityAlgorithms.HmacSha256);
+
+        Claim[] claims = [
+            // sub（subject）クレームは JWT の主語となる主体の識別子である. JWT に含まれるクレームは, 通常 subject について述べたものである.  
+            // https://openid-foundation-japan.github.io/draft-ietf-oauth-json-web-token-11.ja.html#subDef
+            new(JwtRegisteredClaimNames.Sub, userId.Value.ToString()),
+            // jti (JWT ID) クレームは, JWT のための一意な識別子を提供する. その識別子の値は, 重複確率が無視できるほど十分低いことを保証できる方法で割り当てられなければならない
+            // https://openid-foundation-japan.github.io/draft-ietf-oauth-json-web-token-11.ja.html#jtiDef
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        ];
+
+        var token = new JwtSecurityToken(
+            issuer: _issuer,
+            audience: _audience,
+            claims: claims,
+            signingCredentials: credentials,
+            expires: DateTime.UtcNow.AddHours(1)
+        );
+        var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+        return tokenString;
+    }
+
+    /**
+     * [参考]
+     * https://qiita.com/te-k/items/600df0d0d812139de881
+     * https://openid-foundation-japan.github.io/draft-ietf-oauth-json-web-token-11.ja.html
+     */
+}
