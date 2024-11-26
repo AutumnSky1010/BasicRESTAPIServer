d725500f2fcfeae2e84810b6c5cfd1d23cdd853f
AutumnSky1010 <66455966+AutumnSky1010@users.noreply.github.com>
2024-11-26 15:05:36 +0900

    :+1: 認証コントローラを実装

---
+++ API/Server/Controllers/AuthenticationController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using Server.UseCases;
+using Server.UseCases.UserAuthentications;
+
+namespace Server.Controllers;
+
+[ApiController]
+[Route("auth")]
+public class AuthenticationController(ILogger<AuthenticationController> logger, UserAuthenticationUseCase authUseCase) : ControllerBase
+{
+    private readonly ILogger<AuthenticationController> _logger = logger;
+    private readonly UserAuthenticationUseCase _authUseCase = authUseCase;
+
+    public record SignInParams(string SignInId, string Password);
+    public record SignInResponse(string AccessToken);
+    [AllowAnonymous]
+    [EnableRateLimiting(Program.TOKEN_RATE_LIMITER)]
+    [HttpPost("sign_in")]
+    public async Task<IActionResult> RegisterAsync(SignInParams param)
+    {
+        // サインインユースケースを実行
+        var (resultTypes, accessToken) = await _authUseCase.SignInAsync(param.SignInId, param.Password);
+        if (resultTypes is ResultTypes.Success)
+        {
+            return Ok(new SignInResponse(accessToken));
+        }
+
+        if (resultTypes is ResultTypes.InternalError)
+        {
+            _logger.LogError("サインイン時にサーバ内エラーが発生しました");
+            return StatusCode(500, "Internal Server Error");
+        }
+
+        return BadRequest();
+    }
+}
