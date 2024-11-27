700a7b8736417333371588f45e9e004e89a31f53
AutumnSky1010 <66455966+AutumnSky1010@users.noreply.github.com>
2024-11-27 22:59:11 +0900

    :test_tube: モックを利用してユースケースをテスト

---
+++ API/ServerTests/UseCases/Users/TestUserAuthenticationUseCase.cs
@@ -0,0 +1,118 @@
+﻿using Moq;
+using Server.Models.UserAuthentications;
+using Server.Models.Users;
+using Server.UseCases;
+using Server.UseCases.UserAuthentications;
+using Server.UseCases.Users;
+
+namespace ServerTests.UseCases.Users;
+public class TestUserAuthenticationUseCase
+{
+    [Fact(DisplayName = "サインインできるかの正常系テスト")]
+    public async Task SignIn_Valid()
+    {
+        // Arrange
+        // モックを作成
+        var hasher = new Mock<IHasher>();
+        var tokenGenerator = new Mock<IAccessTokenGenerator>();
+        var authRepository = new Mock<IUserAuthenticationRepository>();
+        var userRepository = new Mock<IUserRepository>();
+        var useCase = new UserAuthenticationUseCase(hasher.Object, tokenGenerator.Object, authRepository.Object, userRepository.Object);
+        // データの定義
+        var userInputCurrentPassword = "userInputPass";
+        var storedPassword = StoredPassword.CreateFromString("fromDB");
+        var userInputSignInId = "userInputSignInId";
+        var userNameValue = "user";
+        if (!SignInId.TryCreate(userInputSignInId, out var signInId) ||
+            !UserName.TryCreate(userNameValue, out var userName) ||
+            !RawPassword.TryCreate(userInputCurrentPassword, out var rawPassword))
+        {
+            Assert.Fail("テストデータが不正です");
+            return;
+        }
+        var userId = UserId.CreateNew();
+        var registeredAt = DateTime.Now;
+        var user = User.Create(userId, userName, registeredAt);
+        var expectedAccessToken = "accessToken";
+        // アクセストークン生成の動作を定義
+        tokenGenerator
+            .Setup(tg => tg.Generate(user.Id))
+            .Returns(expectedAccessToken);
+        // ハッシュ化したパスワードを保存済みパスワードと同一のものにする
+        hasher
+            .Setup(h => h.Generate(user, rawPassword))
+            .Returns(storedPassword.Value);
+        // ユーザリポジトリからユーザ情報を受け取る時の動作を定義
+        userRepository
+            .Setup(repo => repo.TryFindUserByIdAsync(userId))
+            .ReturnsAsync((true, user));
+        // ユーザリポジトリからパスワードを受け取る時の動作を定義
+        authRepository
+            .Setup(repo => repo.TryFindAuthenticationAsync(signInId))
+            .ReturnsAsync((true, userId, storedPassword));
+
+        // Act
+        var (resultTypes, actualAccessToken) = await useCase.SignInAsync(userInputSignInId, userInputCurrentPassword);
+
+        // Assert
+        // アクセストークンを生成しているかを確認する。
+        tokenGenerator.Verify(tg => tg.Generate(user.Id), Times.Once());
+        // 戻り値のチェックを行う
+        Assert.Equal(ResultTypes.Success, resultTypes);
+        Assert.Equal(expectedAccessToken, actualAccessToken);
+    }
+
+    [Fact(DisplayName = "サインインできるかの異常系テスト")]
+    public async Task SignIn_Invalid()
+    {
+        // Arrange
+        // モックを作成
+        var hasher = new Mock<IHasher>();
+        var tokenGenerator = new Mock<IAccessTokenGenerator>();
+        var authRepository = new Mock<IUserAuthenticationRepository>();
+        var userRepository = new Mock<IUserRepository>();
+        var useCase = new UserAuthenticationUseCase(hasher.Object, tokenGenerator.Object, authRepository.Object, userRepository.Object);
+        // データの定義
+        var userInputCurrentPassword = "userInputPass";
+        var storedPassword = StoredPassword.CreateFromString("fromDB");
+        var userInputSignInId = "userInputSignInId";
+        var userNameValue = "user";
+        if (!SignInId.TryCreate(userInputSignInId, out var signInId) ||
+            !UserName.TryCreate(userNameValue, out var userName) ||
+            !RawPassword.TryCreate(userInputCurrentPassword, out var rawPassword))
+        {
+            Assert.Fail("テストデータが不正です");
+            return;
+        }
+        var userId = UserId.CreateNew();
+        var registeredAt = DateTime.Now;
+        var user = User.Create(userId, userName, registeredAt);
+        var expectedAccessToken = "accessToken";
+        // アクセストークン生成の動作を定義
+        tokenGenerator
+            .Setup(tg => tg.Generate(user.Id))
+            .Returns(expectedAccessToken);
+        // ユーザリポジトリからユーザ情報を受け取る時の動作を定義
+        userRepository
+            .Setup(repo => repo.TryFindUserByIdAsync(userId))
+            .ReturnsAsync((true, user));
+        // ハッシュ化したパスワードを保存済みパスワードと異なるものにする
+        hasher
+            .Setup(h => h.Generate(user, rawPassword))
+            .Returns("異なるパスワード ");
+        // ユーザリポジトリからパスワードを受け取る時の動作を定義
+        authRepository
+            .Setup(repo => repo.TryFindAuthenticationAsync(signInId))
+            .ReturnsAsync((true, userId, storedPassword));
+
+        // Act
+        var (resultTypes, actualAccessToken) = await useCase.SignInAsync(userInputSignInId, userInputCurrentPassword);
+
+        // Assert
+        // アクセストークンを生成していないことを確認する。
+        tokenGenerator.Verify(tg => tg.Generate(user.Id), Times.Never());
+        // 戻り値のチェックを行う
+        Assert.Equal(ResultTypes.ValidationError, resultTypes);
+        Assert.NotEqual(expectedAccessToken, actualAccessToken);
+    }
+}
---
+++ API/ServerTests/UseCases/Users/TestUserUseCase.cs
@@ -0,0 +1,161 @@
+﻿using Moq;
+using Server.Models.UserAuthentications;
+using Server.Models.Users;
+using Server.UseCases;
+using Server.UseCases.UserAuthentications;
+using Server.UseCases.Users;
+
+namespace ServerTests.UseCases.Users;
+public class TestUserUseCase
+{
+    [Theory(DisplayName = "ユーザを挿入できるかの正常系テスト")]
+    [InlineData("user", "aiueo@test.co.jp", "          ")]
+    public async Task RegisterUser_Valid(string userName, string signInId, string password)
+    {
+        // Arrange
+        var hasher = new Mock<IHasher>();
+        var tokenGenerator = new Mock<IAccessTokenGenerator>();
+        var userRepository = new Mock<IUserRepository>();
+        var authRepository = new Mock<IUserAuthenticationRepository>();
+        var hashedPassword = "hashed";
+        if (!RawPassword.TryCreate(password, out var rawPassword) ||
+            !UserName.TryCreate(userName, out var expectedUserName) ||
+            !SignInId.TryCreate(signInId, out var expectedSignInId))
+        {
+            Assert.Fail($"インラインデータが不正です。{userName},{signInId},{password}");
+            return;
+        }
+        // authRepositoryが値を返さないように設定する
+        authRepository
+            .Setup(r => r.TryFindAuthenticationAsync(expectedSignInId))
+            .ReturnsAsync((false, new(), StoredPassword.Empty));
+        var userId = UserId.CreateNew();
+        var registeredAt = DateTime.Now;
+        var user = User.Create(userId, expectedUserName, registeredAt);
+        // ハッシュ化したパスワードを定義
+        hasher
+            .Setup(h => h.Generate(user, rawPassword))
+            .Returns(hashedPassword);
+        var expectedHashedPassword = HashedPassword.HashFromRawPassword(hasher.Object, rawPassword, user);
+        // リポジトリのユーザ作成メソッドは成功を返すように設定
+        userRepository
+            .Setup(r => r.TryCreateUserAsync(user, expectedSignInId, expectedHashedPassword))
+            .ReturnsAsync(true);
+        var useCase = new UserUseCase(userRepository.Object, authRepository.Object, hasher.Object);
+
+        // Act
+        var (resultTypes, validationResult) = await useCase.RegisterUserAsync(userId, userName, registeredAt, signInId, password);
+
+        // Assert
+        // パスワードハッシュ化メソッドを呼び出しているかをテストする。
+        hasher.Verify(hasher => hasher.Generate(user, rawPassword!));
+        // リポジトリのユーザ作成メソッドが呼び出されている。
+        userRepository.Verify(repo =>
+            repo.TryCreateUserAsync(user, expectedSignInId, expectedHashedPassword),
+            Times.Once());
+        // ユースケースの戻り値で成功した結果が返っているか
+        Assert.Equal(ResultTypes.Success, resultTypes);
+        // バリデーション結果が正しいか
+        Assert.Equal(new UserUseCase.RegisterValidationResult(true, true, true), validationResult);
+    }
+
+    [Theory(DisplayName = "ユーザを挿入できるかの異常系テスト(具体的に不正と判断する基準はモデルのテストで行う)")]
+    // サインインIDが不正
+    [InlineData("user", "", "          ", true, false, true)]
+    // ユーザ名が不正
+    [InlineData("", "aiueo@test.com", "          ", false, true, true)]
+    // パスワードが不正
+    [InlineData("user", "aiueo@test.com", "123456789", true, true, false)]
+    public async Task RegisterUser_Invalid(string userName, string signInId, string password,
+        bool expectedUserNameOk, bool expectedSignInIdOk, bool expectedPasswordOk)
+    {
+        // Arrange
+        var hasher = new Mock<IHasher>();
+        var tokenGenerator = new Mock<IAccessTokenGenerator>();
+        var userRepository = new Mock<IUserRepository>();
+        var authRepository = new Mock<IUserAuthenticationRepository>();
+
+        // authRepositoryが値を返さないように設定する
+        authRepository
+            .Setup(r => r.TryFindAuthenticationAsync(It.IsAny<SignInId>()))
+            .ReturnsAsync((false, new(), StoredPassword.Empty));
+        var userId = UserId.CreateNew();
+        var registeredAt = DateTime.Now;
+        var useCase = new UserUseCase(userRepository.Object, authRepository.Object, hasher.Object);
+
+        // Act
+        var (resultTypes, validationResult) = await useCase.RegisterUserAsync(userId, userName, registeredAt, signInId, password);
+
+        // Assert
+        // リポジトリのユーザ作成メソッドが呼び出されないことを確認
+        userRepository.Verify(repo =>
+            repo.TryCreateUserAsync(It.IsAny<User>(), It.IsAny<SignInId>(), It.IsAny<HashedPassword>()),
+            Times.Never());
+        // ユースケースの結果がバリデーションエラーとなっているかを確認する
+        Assert.Equal(ResultTypes.ValidationError, resultTypes);
+        // バリデーション結果が期待通りかを確認する
+        Assert.Equal(new UserUseCase.RegisterValidationResult(expectedUserNameOk, expectedSignInIdOk, expectedPasswordOk), validationResult);
+    }
+
+    [Fact(DisplayName = "サインインIDが既に存在する場合バリデーションエラーが戻るか")]
+    public async Task TestRegisterUser_ExistsSignInId()
+    {
+        // Arrange
+        var userName = "user";
+        var signInId = "aiueo@test.com";
+        var password = "1234567890";
+        var hasher = new Mock<IHasher>();
+        var tokenGenerator = new Mock<IAccessTokenGenerator>();
+        var userRepository = new Mock<IUserRepository>();
+        var authRepository = new Mock<IUserAuthenticationRepository>();
+        // authRepositoryが値を返すように設定する
+        authRepository
+            .Setup(r => r.TryFindAuthenticationAsync(It.IsAny<SignInId>()))
+            .ReturnsAsync((true, new(), StoredPassword.Empty));
+        var userId = UserId.CreateNew();
+        var registeredAt = DateTime.Now;
+        var useCase = new UserUseCase(userRepository.Object, authRepository.Object, hasher.Object);
+
+        // Act
+        var (resultTypes, validationResult) = await useCase.RegisterUserAsync(userId, userName, registeredAt, signInId, password);
+
+        // Assert
+        // リポジトリのユーザ作成メソッドが呼び出されないことを確認
+        userRepository.Verify(repo =>
+            repo.TryCreateUserAsync(It.IsAny<User>(), It.IsAny<SignInId>(), It.IsAny<HashedPassword>()),
+            Times.Never());
+        // ユースケースの結果がバリデーションエラーとなっているかを確認する
+        Assert.Equal(ResultTypes.ValidationError, resultTypes);
+        // バリデーション結果が期待通りかを確認する
+        Assert.Equal(new UserUseCase.RegisterValidationResult(true, false, true), validationResult);
+    }
+
+    [Fact(DisplayName = "ユーザ作成リポジトリで失敗した場合内部エラーとなるか")]
+    public async Task InsertUser_InternalInvalid()
+    {
+        // Arrange
+        var userName = "user";
+        var signInId = "aiueo@test.com";
+        var password = "1234567890";
+        var hasher = new Mock<IHasher>();
+        var tokenGenerator = new Mock<IAccessTokenGenerator>();
+        var userRepository = new Mock<IUserRepository>();
+        var authRepository = new Mock<IUserAuthenticationRepository>();
+        var repository = new Mock<IUserRepository>();
+        // TryCreateUserに失敗させる。
+        repository.Setup(repo => repo.TryCreateUserAsync(
+            It.IsAny<User>(),
+            It.IsAny<SignInId>(),
+            It.IsAny<HashedPassword>()))
+            .ReturnsAsync(() => false);
+        var userId = UserId.CreateNew();
+        var registeredAt = DateTime.Now;
+        var useCase = new UserUseCase(userRepository.Object, authRepository.Object, hasher.Object);
+
+        // Act
+        var (resultTypes, _) = await useCase.RegisterUserAsync(userId, userName, registeredAt, signInId, password);
+
+        // Assert
+        Assert.Equal(ResultTypes.InternalError, resultTypes);
+    }
+}
